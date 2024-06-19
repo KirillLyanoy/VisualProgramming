6a3c843294cf8de5d6701712caab63f1f0470edc
lyanoy <uwaltonsimons@gmail.com>
2024-06-19 13:56:32 +0700

    Edit Homework3

---
+++ Домашнее задание №3/dz3/Calculator.cs
@@ -0,0 +1,63 @@
+﻿using Avalonia.Input;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz3
+{
+    class Calculator
+    {
+        static private double _x;
+        static private double _y;
+        static private string? _operator;
+        static public void SetX(double x) { _x = x; }
+        static public double GetX() { return _x; }
+        static public void SetY(double y) { _y = y; }
+        static public void SetOperator(string operatorName) { _operator = operatorName; }
+        static public double Calculation()
+        {
+            switch (_operator)
+            {
+                case "Plus":
+                    return (_x + _y);
+                case "Minus":
+                    return (_x - _y);
+                case "Multiply":
+                    return (_x * _y);
+                case "Divide":
+                    return (_x / _y);
+                case "Mod":
+                    return (_x % _y);
+                case "Factorial":                   
+                     long temp = 1;
+                    for (int i = 1; i <= _x; i++)
+                    {
+                        temp *= i;
+                    }
+                    return (temp);
+                case "Degree":
+                    return Math.Pow(_x, _y);
+                case "Log":
+                    return Math.Log10(_x);
+                case "Ln":
+                    return Math.Log(_x);
+                case "Sin":
+                    return Math.Sin(_x);
+                case "Cos":
+                    return Math.Cos(_x);
+                case "Tan":
+                    return Math.Tan(_x);
+                case "Floor":
+                    return Math.Floor(_x);
+                case "Ceil":
+                    return Math.Ceiling(_x);
+                default:
+                    return 0;
+            }
+        }
+    }
+}
--- Домашнее задание №3/dz3/DataContextMainWindow.cs
+++ Домашнее задание №3/dz3/DataContextMainWindow.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -8,8 +9,9 @@ namespace dz3;
 
 public class DataContextMainWindow : INotifyPropertyChanged
 {
-    private string _text = "0";
-
+    private string _text = "0"; //текс в главном текстовом блоке//
+    private bool yIndex = false; //используется для сохранения второго значения при повторном нажатии "="//
+                                 
     public string Text
     {
         get
@@ -27,35 +29,56 @@ public class DataContextMainWindow : INotifyPropertyChanged
     {
         if (parameter is string text)
         {
-            if (_text == "0" && text != ".") Text = text;
+            if (_text == "0" && text != ",") Text = text;
 
             else
             {
-                Text = _text + text;
+                if (_text == "-0") Text = "-" + text;
+                else Text = _text + text;
             }
         }
     }
-
     public void DeleteLastElement()
     {
         int n = Text.Length;
         if (n > 0) Text = Text.Remove(n - 1);
         if (Text.Length == 0) Text = "0";
     }
-
     public void Clear()
     {
-        Text = "0";        
+        Text = "0";
+        Calculator.SetX(0);
+        Calculator.SetY(0);
+    }
+    public void CalculateWithOneElement(string _operator)
+    {
+        Calculator.SetX(Convert.ToDouble(_text));
+        Calculator.SetOperator(_operator);
+        Calculator.SetX(Calculator.Calculation());
+        Text = Convert.ToString(Calculator.GetX());
+    }
+    public void CalculateWithTwoElements(string _operator)
+    {
+        Calculator.SetX(Convert.ToDouble(_text));
+        Text = "0";
+        Calculator.SetOperator(_operator);
+        yIndex = true; 
     }
 
-    public bool CanExecuteCommand(object parameter)
-    {
-        if (parameter is string text)
-        {
-            return text.StartsWith("Hello") == false;
-        }
+    public void Equally()
+    {        
+        if (yIndex) Calculator.SetY(Convert.ToDouble(_text));
+        Calculator.SetX(Calculator.Calculation());
+        Text = Convert.ToString(Calculator.GetX());       
+        yIndex = false;
+    }
 
-        return false;
+    public void NegativeNumber()
+    {
+        
+
+        if (_text[0] == '-') Text = _text.Remove(0, 1);
+        else Text = "-" + _text;
     }
 
     public event PropertyChangedEventHandler? PropertyChanged;
