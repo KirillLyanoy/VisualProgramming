e3734226dccd5b4382eaef494c7fd1edfad6549a
lyanoy <uwaltonsimons@gmail.com>
2024-06-22 20:01:19 +0700

    Add homework4

---
+++ Домашнее задание №4/dz4/MainWindow.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Microsoft.VisualBasic;
+using System;
+using System.IO;
+using Tmds.DBus.Protocol;
+
+namespace dz4
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
