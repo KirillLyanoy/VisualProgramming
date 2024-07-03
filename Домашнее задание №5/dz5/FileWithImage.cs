c96f8844e753c2c9a8140b0b957599a4259962f1
KirillLyanoy <141577305+KirillLyanoy@users.noreply.github.com>
2024-07-03 11:07:16 +0700

    Delete Домашнее задание №5/dz5 directory

--- Домашнее задание №5/dz5/FileWithImage.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace dz5
-{
-    //класс, определяющий картинку и имя файла//
-    internal class FileWithImage : TypeWithImage
-    {
-        public FileWithImage(string name) : base(name) { }
-    }
-}
--- Домашнее задание №5/dz5/FolderWithImage.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace dz5
-{
-    //класс, определяющий картинку и имя папки//
-    internal class FolderWithImage : TypeWithImage
-    {
-        public FolderWithImage(string name) : base(name) { }
-    }
-}
