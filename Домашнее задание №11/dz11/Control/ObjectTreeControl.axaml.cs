a0c0b918288bd6e66c35f75c2a5d2047719911ee
Кирилл Ляной <uwaltonsimons@gmail.com>
2024-08-10 15:22:01 +0700

    Final hw11

--- Домашнее задание №11/dz11/Control/ObjectTreeControl.axaml.cs
+++ Домашнее задание №11/dz11/Control/ObjectTreeControl.axaml.cs
@@ -17,31 +17,5 @@ public class ObjectTreeControl : TemplatedControl
     {
         get => GetValue(CurrentObjectProperty);
         set => SetValue(CurrentObjectProperty, value);
-    }
-
-   
+    }   
 }
-/*
-	<Window.DataTemplates>
-		<DataTemplate DataType="{x:Type con:Leaf}">
-			<Grid ColumnDefinitions="*,*">
-				<TextBox Grid.Column="1" Text="{Binding Value}" IsReadOnly="True"/>
-				<TextBox Grid.Column="0" Text="{Binding PropertyName}" IsReadOnly="True"/>
-			</Grid>
-		</DataTemplate>
-		<DataTemplate DataType="{x:Type con:Component}">
-			<Expander Header="{Binding PropertyName}">
-				<ListBox ItemsSource="{Binding Children}">
-				</ListBox>
-			</Expander>
-		</DataTemplate>		
-	</Window.DataTemplates>
-	
-	
-	<StackPanel Margin="30">
-		<Expander Header="Object Expander">
-			<ListBox ItemsSource="{Binding CurrentUser, Converter={StaticResource ObjectConverter}}" />
-		</Expander>
-	</StackPanel>
-	
- */
